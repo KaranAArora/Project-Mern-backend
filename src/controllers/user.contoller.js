51db1f8eb39494080b74d1f8c27fe8bb27a8b59e
Karan Arora <110733612+KaranAArora@users.noreply.github.com>
2023-12-24 19:26:45 +0530

    Added DB Pipeline for User Channel Data

--- src/controllers/user.contoller.js
+++ src/controllers/user.contoller.js
@@ -413,6 +413,87 @@ const updateUserCoverImage = asyncHandler(async(req, res) =>{
         )
 });
 
+// Getting Channel Profile Logic
+const getUserChannelProfile = asyncHandler(async(req, res) => {
+    // Username from Parms
+    const { username } = req.params;
+
+    if (!username?.trim()) {
+        throw new ApiError(400, "Username is Missing !!");
+    }
+
+    // DB Query
+    const channel = await User.aggregate([
+        {
+            $match : {
+                username : username?.toLowerCase()
+            }
+        },
+        {
+            $lookup : {
+                from : "subscriptions",
+                localField : "_id",
+                foreignField : "channel",
+                as : "subscribers"
+            }
+        },
+        {
+            $lookup : {
+                from : "subscriptions",
+                localField : "_id",
+                foreignField : "subscriber",
+                as : "subscribedTo"
+            }
+        },
+        {
+            $addFields : {
+                subscribersCount : {
+                    $size : "$subscribers"
+                },
+                channelsSubscribedToCount : {
+                    $size : "$subscribedTo"
+                },
+                isSubscribed : {
+                    $cond : {
+                        if : { $in : [req.user?._id, "$subscribers.subscriber"]},
+                        then : true,
+                        else : false
+                    }
+                }
+            }
+        },
+        {
+            $project : {
+                email: 1,
+                username: 1,
+                fullName: 1,
+                subscribersCount: 1,
+                channelsSubscribedToCount: 1,
+                isSubscribed: 1,
+                avatar: 1,
+                coverImage: 1
+            }
+        }
+    ]);
+
+    if (!channel?.length) {
+        throw new ApiError(404, "Channel Does Not Exists !!");
+    }
+
+    console.log(channel);
+
+    //Returning Response
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                channel[0],
+                "Channel Data Fetched Successfully !!"
+            )
+        )
+})
+
 
 //Export
 export {
@@ -424,5 +505,6 @@ export {
     getCurrentUser,
     updateAccountDetails,
     updateUserAvatar,
-    updateUserCoverImage
+    updateUserCoverImage,
+    getUserChannelProfile
 }
